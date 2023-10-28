7d5fc8cd50dec3840c004c9114c6e9387a2bc01e
Adrish Mitra <adrishmitra710@gmail.com>
2023-10-29 00:50:24 +0530

    company model export statement added

--- Models/CompanyModel.js
+++ Models/CompanyModel.js
@@ -1,24 +1,31 @@
 const mongoose = require ('mongoose')
-const companyModel = new mongoose.Schema ({
-name:{
-    type:String,
-    require:true,
-    trim:true
-},
-logo: {
-    data :Buffer,
-    contentType: String
-},
-JobList: [{
+
+const companyModel = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    logo: {
+        data :Buffer,
+        contentType: String
+    },
+    job_lists: [{
+        type: String
+    }],
+    company_description : {
+        type: String
+    },
+    social_links : [{
+        type: String
+    }],
+    industry :{
     type: String
-}],
-About_company : {
-    type: String
-},
-social_links : [{
-    type: String
-}],
-Industry :{
-type: String
-}
+    }
 })
+
+
+
+const CompanyModel = mongoose.model('CompanyModel', companyModel)
+
+module.exports = CompanyModel
